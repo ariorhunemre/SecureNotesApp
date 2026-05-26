396cab8888b57e82462893e309bf71ef2f0957ff
ariorhunemre <ariorhunemre@gmail.com>
2026-05-26 17:53:16 +0300

    Initial project setup

---
+++ SecureNotesApp/src/main/java/com/securenotes/Main.java
@@ -0,0 +1,13 @@
+package com.securenotes;
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Secure Notes App");
+		frame.setSize(500, 400);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+	}
+}
